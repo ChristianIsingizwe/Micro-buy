4c9fbd4886f26a0c8030dfc34ffe249891af7c4d
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-07-16 09:09:45 +0200

    Connecting to the database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import config from 'config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(config.get('mongodbUrl'), {
+      w: 1,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
