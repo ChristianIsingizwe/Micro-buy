e46f50a2d2cf728963e396deabfd9b433e016076
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-07-16 11:07:22 +0200

    Added user resource

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import  config from 'config'
 import { allExceptionFilter } from './httpExceptionFilter';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(config.get<string>("mongodbUrl"), {
       w: 1,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [
