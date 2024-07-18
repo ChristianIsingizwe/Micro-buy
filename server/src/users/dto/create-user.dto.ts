e31a2a1b927ab547397f9a68e5f92957ad04e363
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-07-18 15:52:21 +0200

    Updating the user

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,8 +1,25 @@
+import { IsIn, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { userTypes } from 'src/shared/schema/userSchema';
+
 export class CreateUserDto {
-    name: string
-    email: string
-    password: string
-    type: string
-    secretToken: string
-    
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsIn([userTypes.ADMIN, userTypes.CUSTOMER])
+  type: string;
+
+  @IsString()
+  @IsOptional()
+  secretToken?: string;
 }
