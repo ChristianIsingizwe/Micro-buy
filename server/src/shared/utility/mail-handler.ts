6ca1b94aa27edca0afcb5fd3f0d41f3389fecbd7
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-07-19 07:09:52 +0200

    Some changes made

--- server/src/shared/utility/mail-handler.ts
+++ server/src/shared/utility/mail-handler.ts
@@ -1,14 +1,24 @@
 import FormData from 'form-data'
+import config from 'config'
 
 export const sendEmail = async (
   to: string,
   templateName: string,
   subject: string,
-  data: Record<string, any> = {},
+  templateVars: Record<string, any> = {},
 ) => {
    const form = new FormData
    form.append('to', to)
    form.append('template', templateName)
    form.append('subject', subject)
-   form.append()
+   form.append(
+    'from',
+    'sandbox271bf24d5254442aa644f51306b55851.mailgun.org'
+   )
+   Object.keys(templateVars).forEach((key)=>{
+    form.append(`v:${key}`, templateVars[key])
+   })
+
+   const username = 'api'
+   const password= config.get('emailService.privateKey')
 };
